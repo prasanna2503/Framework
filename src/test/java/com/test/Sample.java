acdcc260b8e4e59314affbd0e95bf8816df18173
prasanna2503 <kprasanna2406@gmail.com>
2024-09-17 23:41:00 +0530

    How to write excel in selenium

--- src/test/java/com/test/Sample.java
+++ src/test/java/com/test/Sample.java
@@ -1,14 +1,64 @@
 package com.test;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.List;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
 public class Sample {
-	public static void main(String[] args) {
-		Employee e1 = Employee.getObject();
-		e1.getEmpName();
-		e1.getAddress();
-		System.out.println(System.identityHashCode(e1));
-		Employee e2 = Employee.getObject();
-		e2.getEmpName();
-		e2.getAddress();
-		System.out.println(System.identityHashCode(e2));
-	}
+	public static void main(String[] args) throws IOException {
+		File file = new File("C:\\Users\\LENOVO\\eclipse-workspace\\FrameClass\\Excel Sheets\\ExcelWrite.xlsx");
+		
+		Workbook workbook = new XSSFWorkbook();
+		
+		Sheet sheet = workbook.createSheet("Sheet1");
+		
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://omrbranch.com/apitestingtraininginchennaiomr");
+		
+		driver.manage().window().maximize();
+		
+		WebElement element = driver.findElement(By.id("country-list"));
+		Select select = new Select(element);
+		List<WebElement> options = select.getOptions();
+		int size = options.size();
+		System.out.println(size);
+		
+		for (int i=0; i < options.size(); i++) {
+			
+			Row row = sheet.createRow(i);
+			
+			Cell cell = row.createCell(0);
+			
+			WebElement countryList = options.get(i);
+			
+			String text = countryList.getText();
+			
+			cell.setCellValue(text);
+			
+			
+			
+		}
+		
+		FileOutputStream out = new FileOutputStream(file);
+		
+		workbook.write(out);		
+		
+		
+		
+	}	
+		
 }
+
